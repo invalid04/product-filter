bff3ada1bdaa5fff5a3fc366d496009934e58ad0
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-20 00:25:36 -0500

    displaying products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from 'axios'
 import { QueryResult } from "@upstash/vector";
-import { Product } from "@/db";
+import type { Product as TProduct } from "@/db";
+import Product from "@/components/Products/Product";
 
 const SORT_OPTIONS =[
   {name: 'None', value: 'none'},
@@ -26,7 +27,7 @@ export default function Home() {
   const { data: products } = useQuery({
     queryKey: ['products'],
     queryFn: async () => {
-      const { data } = await axios.post<QueryResult<Product>[]>(
+      const { data } = await axios.post<QueryResult<TProduct>[]>(
         'http://localhost:3000/api/products', {
           filter: {
             sort: filter.sort,
@@ -89,8 +90,8 @@ export default function Home() {
           <div></div>
 
           <ul className='lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
-            {products?.map(() => (
-              <Product />
+            {products?.map((product) => (
+              <Product product={product.metadata!} />
             ))}
           </ul>
         </div>
