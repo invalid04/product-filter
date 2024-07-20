f5d7d174107f77f76faf1e871fe82b152d20893a
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-19 22:56:33 -0500

    dropdown button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           <DropdownMenu>
             <DropdownMenuTrigger className='group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900'>
               Sort
+              <ChevronDown className='-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500' />
             </DropdownMenuTrigger>
           </DropdownMenu>
         </div>
