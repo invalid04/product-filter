ff4793a21fc24004e0b9ea7e7541204ed70d1861
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-21 23:06:22 -0500

    displaying all colors in filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,7 +133,21 @@ export default function Home() {
                   </AccordionTrigger>
                   <AccordionContent className='pt-6 animate-none'>
                     <ul className='space-y-4'>
-                      {}
+                      {COLOR_FILTERS.options.map((option, optionIdx) => (
+                        <li key={option.value} className='flex items-center'>
+                          <input 
+                            type='checkbox' 
+                            id={`color-${optionIdx}`} 
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' 
+                          />
+                          <label 
+                            htmlFor={`color-${optionIdx}`}
+                            className='ml-3 text-sm text-gray-600'
+                          >
+                            {option.label}
+                          </label>
+                        </li>
+                      ))}
                     </ul>
                   </AccordionContent>
                 </AccordionItem>
