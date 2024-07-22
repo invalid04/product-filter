e10610ec59c8d19fe5e79a759f4879b1b5b3e3fe
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-21 23:15:32 -0500

    available colors const

--- src/lib/validators/product-validator.ts
+++ src/lib/validators/product-validator.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod'
 
 export const AVAILABLE_SIZES = ['S', 'M', 'L'] as const 
+export const AVAILABLE_COLORS = ['white', 'beige', 'green', 'purple', 'blue'] as const 
 
 export const ProductFilterValidator = z.object({
-    size: z.array(z.enum())
+    size: z.array(z.enum(AVAILABLE_SIZES)),
+    color: z.array(z.enum(AVAILABLE_COLORS))
 })
