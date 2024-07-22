198dd244f85b732464a8a5e34d8b86e9730efcf1
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-22 00:09:02 -0500

    price filters const

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,16 @@ const SIZE_FILTERS = {
   ] 
 } as const
 
+const PRICE_FILTERS = {
+  id: 'price',
+  name: 'Price',
+  options: [
+    {value: [0, 100], label: 'Any Price'},
+    {value: [0, 20], label: 'Under $20'},
+    {value: [0, 40], label: 'Under $40'},
+  ]
+} as const
+
 const DEFAULT_CUSTOM_PRICE = [0, 100] as [number, number]
 
 export default function Home() {
@@ -198,6 +208,41 @@ export default function Home() {
                   <AccordionTrigger className='py-3 text-sm text-gray-400 hover:text-gray-500'>
                     <span className='font-medium text-gray-900'>
                       Size
+                    </span>
+                  </AccordionTrigger>
+                  <AccordionContent className='pt-6 animate-none'>
+                    <ul className='space-y-4'>
+                      {SIZE_FILTERS.options.map((option, optionIdx) => (
+                        <li key={option.value} className='flex items-center'>
+                          <input 
+                            type='checkbox' 
+                            id={`size-${optionIdx}`} 
+                            onChange={() => {
+                              applyArrayFilter({
+                                category: 'size',
+                                value: option.value
+                              })
+                            }}
+                            checked={filter.size.includes(option.value)}
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500' 
+                          />
+                          <label 
+                            htmlFor={`color-${optionIdx}`}
+                            className='ml-3 text-sm text-gray-600'
+                          >
+                            {option.label}
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+
+                {/* Price Filter */}
+                <AccordionItem value='price'>
+                  <AccordionTrigger className='py-3 text-sm text-gray-400 hover:text-gray-500'>
+                    <span className='font-medium text-gray-900'>
+                      Price
                     </span>
                   </AccordionTrigger>
                   <AccordionContent className='pt-6 animate-none'>
